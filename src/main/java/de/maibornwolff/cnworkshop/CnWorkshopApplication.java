8c439d0967631efc130e9a483258ee3a20db287a
bartosz.boron <bartosz.boron@maibornwolff.de>
2019-12-11 15:57:46 +0100

    initial setup

---
+++ src/main/java/de/maibornwolff/cnworkshop/CnWorkshopApplication.java
@@ -0,0 +1,13 @@
+package de.maibornwolff.cnworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnWorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/maibornwolff/cnworkshop/CnWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package de.maibornwolff.cnworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
